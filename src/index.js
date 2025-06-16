e5fff4d67ce0407e3646247043164a71aa2450ad
MOHANRAJ R <mohanraj.r.personal@gmail.com>
2025-06-16 10:19:10 +0530

    Solved comment line conflict in index.js

--- src/index.js
+++ src/index.js
@@ -10,11 +10,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
-    
-=======
->>>>>>> 836c0bddd078a3c639beeb7c3dd2c901969952c9
-    {/* <UserDashboard />
-    <AdminDashboard /> */}
   </React.StrictMode>
 );
